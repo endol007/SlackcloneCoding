685d206ecfb15333f34b55329d0b67f2c5869355
seanstainability <seanstainability@gmail.com>
2021-07-19 11:03:34 +0900

    channel socket 테스트용 코드 작성

--- src/pages/Channels.js
+++ src/pages/Channels.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import useSocket from "../useSocket";
+import { useParams } from "react-router";
 
 import ChatHeader from "../components/ChatHeader";
 import ChatList from "../components/ChatList";
 import ChatBox from "../components/ChatBox";
 
 const Channels = (props) => {
+  const { channel } = useParams();
+  const [socket] = useSocket(channel);
+
+  useEffect(() => {
+    socket?.on("message", onMessage);
+    return () => {
+      socket?.off("dm", onMessage);
+    };
+  }, [socket]);
+
+  const onMessage = (data) => {
+    console.log("message", data);
+  };
+
   return (
     <React.Fragment>
       <ChatHeader></ChatHeader>
