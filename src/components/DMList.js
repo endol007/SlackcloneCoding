2fa77d1cb132a7ccca518e18959a1162cd4f1d0d
seanstainability <seanstainability@gmail.com>
2021-07-21 14:42:44 +0900

    backend 요청사항 반영

--- src/components/DMList.js
+++ src/components/DMList.js
@@ -3,64 +3,18 @@ import { ArrowDropDown, ArrowDropUp } from "@styled-icons/material-outlined";
 import { CollapseButton } from "./ChannelList";
 import DM from "./DM";
 import { useSelector, useDispatch } from "react-redux";
-import { getDmUsers } from "../redux/async/dm";
-import { useParams } from "react-router-dom";
-import useSocket from "../useSocket";
-import { createDM } from "../redux/async/dm";
+import { getDMList } from "../redux/async/dm";
 
 const DMList = ({ currentUser }) => {
-  const { dmsId } = useParams();
   const dispatch = useDispatch();
   const [collapse, setCollapse] = useState(true);
   const { dmList } = useSelector((state) => state.dm);
-  const [socket] = useSocket(dmsId);
-
-  // const dmList = [
-  //   {
-  //     id: 1,
-  //     User: {
-  //       id: 4,
-  //       email: "sean@gamil.com",
-  //       nickname: "sean",
-  //     },
-  //     OtherUser: {
-  //       id: 4,
-  //       email: "sean@gmail.com",
-  //       nickname: "sean",
-  //     },
-  //   },
-  //   {
-  //     id: 2,
-  //     User: {
-  //       id: 4,
-  //       email: "sean@gamil.com",
-  //       nickname: "sean",
-  //     },
-  //     OtherUser: {
-  //       id: 2,
-  //       email: "sparta@gmail.com",
-  //       nickname: "sparta",
-  //     },
-  //   },
-  // ];
 
   useEffect(() => {
     if (currentUser) {
-      // console.log("getDmUsers", currentUser.id);
-      dispatch(getDmUsers({ userId: currentUser.id }));
+      dispatch(getDMList({ userId: currentUser.id }));
     }
   }, [currentUser]);
-
-  useEffect(() => {
-    socket?.on("dm", (data) => {
-      console.log("dm", data);
-    });
-    console.log("socket on dm", socket?.hasListeners("dm"), socket);
-    return () => {
-      console.log("socket off dm", socket?.hasListeners("dm"));
-      socket?.off("dm");
-    };
-  }, [socket]);
 
   const toggleCollapse = useCallback(() => {
     setCollapse((prev) => !prev);
